6fa3f50199536b40712be8fe8ba820a6995f1c2f
aryan <aryan2563737@gmail.com>
2021-08-17 21:58:03 +0530

    User data model modified

--- src/Components/Dashboard/Form.tsx
+++ src/Components/Dashboard/Form.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react"
 import { useRef } from "react"
 import { createUserEndpoint } from "../../API_Endpoints"
 import Page1 from "./Page1"
